35fdd53bbe1b3f5756732e6739a7fd382dd8c39b
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2024-02-08 11:05:06 +0200

    modified the class to be more c++11 compitable

---
+++ module05/ex00/inc/Bureaucrat.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/19 07:31:02 by joonasmykka       #+#    #+#             */
+/*   Updated: 2024/02/08 11:04:21 by jmykkane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# define GRADE_HIGH 1
+# define GRADE_LOW 150
+
+# include <iostream>
+# include <string>
+
+class Bureaucrat {
+	
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const noexcept override;
+		};
+		
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const noexcept override;
+		};
+		
+		Bureaucrat( void );
+		Bureaucrat( std::string const & newName, int newGrade );
+		Bureaucrat( Bureaucrat const & other );
+		Bureaucrat& operator=( Bureaucrat const & other );
+		~Bureaucrat( void );
+
+		std::string const & getName( void ) const;
+		int					getGrade( void ) const;
+
+		void				incrementGrade( void );
+		void				decrementGrade( void );
+
+	private:
+		std::string	const	_name;
+		int		_grade;
+};
+
+std::ostream& operator<<( std::ostream& os, Bureaucrat const & obj );
+
+#endif // !BUREAUCRAT_HPP
