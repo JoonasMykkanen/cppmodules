16778ac7f027c98a81fcd3cee96364021c994020
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2024-02-19 07:53:45 +0200

    module05 ok

---
+++ module05/ex03/src/Intern.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/17 14:57:30 by jmykkane          #+#    #+#             */
+/*   Updated: 2024/02/19 07:49:49 by jmykkane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Intern.hpp"
+
+Intern::Intern( void ) {
+	// std::cout << "Intern default constructor called \n";
+}
+
+Intern::~Intern( void ) {
+	// std::cout << "Intern default destructor called \n";
+}
+
+Intern::Intern( Intern const & other ) {
+	// std::cout << "Intern copy constructor called \n";
+	*this = other;
+}
+
+Intern& Intern::operator=( Intern const & other ) {
+	// std::cout << "Intern copy assigment operator called \n";
+	( void ) other;
+	return *this;
+}
+
+AForm* Intern::makeRobotomy( std::string target ) {
+	return new RobotomyRequestForm(target);
+}
+
+AForm* Intern::makeShrubbery( std::string target ) {
+	return new ShrubberyCreationForm(target);
+}
+
+AForm* Intern::makePresidental( std::string target ) {
+	return new PresidentalPardonForm(target);
+}
+
+AForm*	Intern::makeForm( std::string const formName, std::string const targetName ) {
+	std::string forms[] = {
+		"RobotomyRequestForm",
+		"ShrubberyCreationForm",
+		"PresidentalPardonForm"
+	};
+
+	Intern::t_forms formFunctions = {
+		&Intern::makeRobotomy,
+		&Intern::makeShrubbery,
+		&Intern::makePresidental
+	};
+
+	AForm* newForm = nullptr;
+	for (int i = 0; i < 3; i++) {
+		if (formName == forms[i]) {
+			newForm = (this->*formFunctions[i])(targetName);
+			std::cout << "Intern creates " << targetName << std::endl;
+			return newForm;
+		}
+	}
+	
+	throw UnknownFormException();
+}
+
+const char* Intern::UnknownFormException::what() const noexcept {
+	return "Form does not exist";
+}
