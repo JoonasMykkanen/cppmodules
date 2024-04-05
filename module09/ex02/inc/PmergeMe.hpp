28c757d5ba5470583601eb35b604b5c593d2a60d
JoonasMykkanen <42795051+JoonasMykkanen@users.noreply.github.com>
2024-04-05 07:56:07 +0300

    made prints nicer

--- module09/ex02/inc/PmergeMe.hpp
+++ module09/ex02/inc/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:57:40 by asarikha          #+#    #+#             */
-/*   Updated: 2024/04/02 10:39:07 by jmykkane         ###   ########.fr       */
+/*   Updated: 2024/04/05 07:50:28 by jmykkane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <list>
 
 # define MICROSECOND 100000
+# define PRINT_PRECISION 5
 
 class PmergeMe
 {		
@@ -28,17 +29,26 @@ class PmergeMe
 		PmergeMe &operator=( const PmergeMe& other );
 		~PmergeMe(void);
 
+		void printArg( int argc, char **argv, std::string type );
 		void sortVector( int argc, char **argv );
 		void sortList( int argc, char **argv );
 
-		void printArg( int argc, char **argv, std::string type );
+	private:
+		std::vector<unsigned int> 	_veccerson;
+		std::list<unsigned int>		_listerson;
+
+		// Vector functions
+		void mergeVector(int start , int mid, int end);
+		void insertionSortVec(int start, int end );
+		void mergeInsertVec(int start, int end );
 		void printVector( void );
+
+		// List functions
+		void mergeList(int start , int mid, int end);
+		void insertionSortList(int start, int end );
+		void mergeInsertList(int start, int end );
 		void printList( void );
-	
-		private:
-			std::vector<unsigned int> 	_veccerson;
-			std::list<unsigned int>		_listerson;
-			
+
 };
 
 
--- module09/ex02/src/PmergeMe.cpp
+++ module09/ex02/src/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: jmykkane <jmykkane@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:57:38 by asarikha          #+#    #+#             */
-/*   Updated: 2024/04/02 10:54:48 by jmykkane         ###   ########.fr       */
+/*   Updated: 2024/04/05 07:53:24 by jmykkane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,23 @@ PmergeMe& PmergeMe::operator=( const PmergeMe& other ){
 	return *this;
 }
 
-static void mergeVector(std::vector<unsigned int>& vec, int start , int mid, int end)
-{
+void PmergeMe::printArg( int argc, char **argv, std::string type ) {
+	int len = argc;
+	if (len > PRINT_PRECISION)
+		len = PRINT_PRECISION;
+	
+	std::cout << "<" << type << ">Before: ";
+	for (int i = 1; i < len; i++) {
+		std::cout << argv[i] << " ";
+	}
+	
+	if (argc > PRINT_PRECISION )
+		std::cout << "[...]" << std::endl;
+	else
+		std::cout << std::endl;
+}
+
+void PmergeMe::mergeVector(int start , int mid, int end) {
     int n1 = mid - start + 1; 
 	int n2 = end - mid; 
 
@@ -36,70 +51,59 @@ static void mergeVector(std::vector<unsigned int>& vec, int start , int mid, int
 
 	// copy from start to mid into left
 	for (int i = 0; i < n1; i++) {
-		Left[i] = vec[start + i];
+		Left[i] = _veccerson[start + i];
 	}
 
     // copy from mid to end into right
 	for (int i = 0; i < n2; i++) {
-		Right[i] = vec[mid + 1 + i];
+		Right[i] = _veccerson[mid + 1 + i];
 	}
 
-	int RIDX = 0;
-	int LIDX = 0;
-
+	int RIGHT_INDEX = 0;
+	int LEFT_INDEX = 0;
 	// sorting+merging the split arrays into vec, starts each round at 0
-	for (int i = start; i <= end; i++) 
-	{
-		if (RIDX == n2)
-		{
-			vec[i] = Left[LIDX];
-			LIDX++;
+	for (int i = start; i <= end; i++) {
+		if (RIGHT_INDEX == n2) {
+			_veccerson[i] = Left[LEFT_INDEX];
+			LEFT_INDEX++;
 		}
-		else if (LIDX == n1)
-		{
-			vec[i] = Right[RIDX];
-			RIDX++;
+		else if (LEFT_INDEX == n1) {
+			_veccerson[i] = Right[RIGHT_INDEX];
+			RIGHT_INDEX++;
 		}
-		else if (Right[RIDX] > Left[LIDX])
-		{
-			vec[i] = Left[LIDX];
-			LIDX++;
+		else if (Right[RIGHT_INDEX] > Left[LEFT_INDEX]) {
+			_veccerson[i] = Left[LEFT_INDEX];
+			LEFT_INDEX++;
 		}
-		else
-		{
-			vec[i] = Right[RIDX];
-			RIDX++;
+		else {
+			_veccerson[i] = Right[RIGHT_INDEX];
+			RIGHT_INDEX++;
 		}
 	}
 }
 
-static void insertionSortVec(std::vector<unsigned int>& vec, int start, int end ) {
-    
-    for (int i = start; i < end; i++)
-	{
-        unsigned int tempVal = vec[i + 1];
+void PmergeMe::insertionSortVec(int start, int end ) {
+    for (int i = start; i < end; i++) {
+        unsigned int tempVal = _veccerson[i + 1];
         int j = i + 1;
-        while (j > start && vec[j - 1] > tempVal) // if curr pos is bigger than next pos
+        while (j > start && _veccerson[j - 1] > tempVal) // if curr pos is bigger than next pos
 		{
-            vec[j] = vec[j - 1];
+            _veccerson[j] = _veccerson[j - 1];
             j--; // count down till 0
         }
-        vec[j] = tempVal;
+        _veccerson[j] = tempVal;
 	}
 }
 
-static void mergeInsertVec(std::vector<unsigned int>& vec, int start, int end ) {
-		
-	if (end - start > 2)
-	{
+void PmergeMe::mergeInsertVec(int start, int end ) {
+	if (end - start > 2) {
 		int mid = (start + end) / 2;
-		mergeInsertVec(vec, start, mid);
-		mergeInsertVec(vec, mid + 1, end); 
-		mergeVector(vec, start, mid, end); // merge sort
+		mergeInsertVec(start, mid);
+		mergeInsertVec(mid + 1, end); 
+		mergeVector(start, mid, end);
 	}
-	else
-	{
-		insertionSortVec(vec, start, end);
+	else {
+		insertionSortVec(start, end);
 	}
 }
 
@@ -107,52 +111,49 @@ void PmergeMe::sortVector(int argc, char **argv){
 	printArg(argc, argv, "vector");
 	std::clock_t start = std::clock();
 
+	_veccerson.reserve(argc);
 	for (int i = 1; i < argc; i++) {
 		_veccerson.push_back(std::stoul(argv[i]));
 	}
-	mergeInsertVec(_veccerson, 0, _veccerson.size() - 1);
+	mergeInsertVec(0, _veccerson.size() - 1);
 
 	double time_taken = static_cast<double>(std::clock() - start) / static_cast<double>(CLOCKS_PER_SEC) * MICROSECOND;
 
 	printVector();
 	std::cout << "Time to process a range of " << argc - 1
-		 << " elements " << "with std::vector<unsigned int> : " 
+		 << " elements " << "with std::vector<>: " 
 		 << time_taken << " microsecond" << std::endl;
-}
-
-
-
-void PmergeMe::printArg( int argc, char **argv, std::string type ) {
-	int len = argc;
-	if (len > 10)
-		len = 10;
-	
-	std::cout << "<" << type << ">Before:	";
-	for (int i = 1; i < len; i++) {
-		std::cout << argv[i] << " ";
-	}
-	std::cout << "[...]" << std::endl;
 }
 
 
 void PmergeMe::printList( void ) {
 	std::list<unsigned int>::const_iterator it;
+	size_t	counter = 0;
 	
-	std::cout << "<list>After:	 ";
-	for (it = _listerson.begin(); it != _listerson.end(); it++) {
+	std::cout << "<list>After:  ";
+	for (it = _listerson.begin(); it != _listerson.end() && counter < PRINT_PRECISION; it++, counter++) {
 		std::cout << *it << " ";
 	}
-	std::cout << "[...]" << std::endl;
+
+	if (_listerson.size() > PRINT_PRECISION)
+		std::cout << "[...]" << std::endl;
+	else
+		std::cout << std::endl;
 }
 
 void PmergeMe::printVector( ) {
 	std::vector<unsigned int>::const_iterator it;
+	size_t	counter = 0;
 	
-	std::cout << "<vector>After: ";
-	for (it = _veccerson.begin(); it != _veccerson.end(); it++) {
+	std::cout << "<vector>After:  ";
+	for (it = _veccerson.begin(); it != _veccerson.end() && counter < PRINT_PRECISION; it++, counter++) {
 		std::cout << *it << " ";
 	}
-	std::cout << "[...]" << std::endl;
+
+	if (_veccerson.size() > PRINT_PRECISION)
+		std::cout << "[...]" << std::endl;
+	else
+		std::cout << std::endl;
 }
 
 static std::list<unsigned int>::iterator advance(std::list<unsigned int>::iterator it, int n) {
@@ -160,7 +161,8 @@ static std::list<unsigned int>::iterator advance(std::list<unsigned int>::iterat
         for (int i = 0; i < n; ++i) {
             ++it;
         }
-    } else if (n < 0) {
+    }
+	else if (n < 0) {
         for (int i = 0; i > n; --i) {
             --it;
         }
@@ -168,8 +170,7 @@ static std::list<unsigned int>::iterator advance(std::list<unsigned int>::iterat
 	return it;
 }
 
-static void mergeList(std::list<unsigned int>& list, int start , int mid, int end){
-
+void PmergeMe::mergeList(int start , int mid, int end) {
 	int n1 = mid - start + 1; 
 	int n2 = end - mid;
 
@@ -179,64 +180,65 @@ static void mergeList(std::list<unsigned int>& list, int start , int mid, int en
 
 	// fill the halfes with the numbers (0 till half and half till end)
 	for (int i = 0; i < n1; i++) { 
-		Left.push_back(*advance(list.begin(), start + i)); // std:next : gives next iterator, at pos n after the start pos
+		Left.push_back(*advance(_listerson.begin(), start + i)); // std:next : gives next iterator, at pos n after the start pos
 	}
 
 	for (int i = 0; i < n2; i++) {
-		Right.push_back(*advance(list.begin(), mid + 1 + i));
+		Right.push_back(*advance(_listerson.begin(), mid + 1 + i));
 	}
 
-	int RIDX = 0;
-	int LIDX = 0;
+	int RIGHT_INDEX = 0;
+	int LEFT_INDEX = 0;
 	// sorting+merging the split arrays into int_vec, starts each round at 0
 	for (int i = start; i <= end; i++) {
-		if (RIDX == n2) {
-			*advance(list.begin(), i) = Left.front(); // front returns reference to first element in the list
+		if (RIGHT_INDEX == n2) {
+			*advance(_listerson.begin(), i) = Left.front(); // front returns reference to first element in the list
 			Left.pop_front();
-			LIDX++;
+			LEFT_INDEX++;
 		}
-		else if (LIDX == n1) {
-			*advance(list.begin(), i) = Right.front();
+		else if (LEFT_INDEX == n1) {
+			*advance(_listerson.begin(), i) = Right.front();
 			Right.pop_front();
-			RIDX++;
+			RIGHT_INDEX++;
 		}
 		else if (Right.front() > Left.front()) {
-			*advance(list.begin(), i) = Left.front(); // insert front element from left array into list at post after i;
+			*advance(_listerson.begin(), i) = Left.front(); // insert front element from left array into list at post after i;
 			Left.pop_front(); // then take it out
-			LIDX++;
+			LEFT_INDEX++;
 		}
 		else {
-			*advance(list.begin(), i) = Right.front();
+			*advance(_listerson.begin(), i) = Right.front();
 			Right.pop_front();
-			RIDX++;
+			RIGHT_INDEX++;
 		}
 	}
 }
 
-static void insertionSortList(std::list<unsigned int>& list, int start, int end ) {
-	
+void PmergeMe::insertionSortList(int start, int end ) {
 	for (int i = start; i < end; i++) {
-        unsigned int tempVal = *advance(list.begin(), i + 1);
+        unsigned int tempVal = *advance(_listerson.begin(), i + 1);
         int j = i + 1;
-        while (j > start && *advance(list.begin(), j - 1) > tempVal) // if curr pos is bigger than next pos
+		// if curr pos is bigger than next pos
+        while (j > start && *advance(_listerson.begin(), j - 1) > tempVal) 
 		{
-            *advance(list.begin(), j) = *advance(list.begin(), j - 1);
+            *advance(_listerson.begin(), j) = *advance(_listerson.begin(), j - 1);
             j--; // count down till 0
         }
-        *advance(list.begin(), j) = tempVal; //insert element at correct position
+		//insert element at correct position
+        *advance(_listerson.begin(), j) = tempVal; 
 	}
 }
 
-static void mergeInsertList(std::list<unsigned int>& list, int start, int end ) {
+void PmergeMe::mergeInsertList(int start, int end ) {
 	// divide into paired groups until num of groups wanted reached
 	if (end - start > 2) {
 		int mid = ( start + end ) / 2;
-		mergeInsertList(list, start, mid);
-		mergeInsertList(list, mid + 1, end); 
-		mergeList(list, start, mid, end);
+		mergeInsertList(start, mid);
+		mergeInsertList(mid + 1, end); 
+		mergeList(start, mid, end);
 	}
 	else {
-		insertionSortList(list, start, end);
+		insertionSortList(start, end);
 	}
 }
 
@@ -246,13 +248,13 @@ void PmergeMe::sortList(int argc, char **argv) {
 
 	for (int i = 1; i < argc; i++)
 		_listerson.push_back(std::stoul(argv[i]));
-	mergeInsertList(_listerson, 0, _listerson.size() - 1);
+	mergeInsertList(0, _listerson.size() - 1);
 
 	double time_taken = static_cast<double>(std::clock() - start) / static_cast<double>(CLOCKS_PER_SEC) * MICROSECOND;
 
 	printList();
 	std::cout << "Time to process a range of " << argc - 1
-		 << " elements " << "with std::list<unsigned int> : " 
+		 << " elements " << "with std::list<> : " 
 		 << time_taken << " microsecond" << std::endl;
 	
 }
